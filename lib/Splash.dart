de7fb88887e936e5b2e2a494f7219af223ab401c
CesarUrquizo018 <cesar.urquizo@unmsm.edu.pe>
2023-09-23 13:04:40 -0500

    v1

---
+++ lib/Splash.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:trabajomovilesg5/Login.dart';
+import 'package:trabajomovilesg5/main.dart';
+
+class MyAppSplash extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title:'SPI-FISI ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePageSplash(),
+    );
+
+  }
+}
+
+class MyHomePageSplash extends StatefulWidget
+{
+  @override
+  State<StatefulWidget>createState()
+  {
+    return _MyHomePageState();
+  }
+
+}
+
+class _MyHomePageState extends State<MyHomePageSplash>
+{
+  @override
+  void initState()
+  {
+    super.initState();
+    _navigatehome();
+  }
+  _navigatehome()async
+  {
+    await Future.delayed(Duration(milliseconds:2000),(){});
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder:(context)=>
+                Login()
+        )
+    );
+  }
+
+  @override
+  Widget build (BuildContext context)
+  {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 320,
+                  height: 320,
+                  color: Colors.white,
+                  child: Image.asset('assets/unmsm_logo.jpg'),
+                )
+              ],
+            )
+        )
+    );
+  }
+
+}
+
+/*class MyHomePage extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    final titulo = 'validacion';
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text(titulo)),
+        )
+    );
+  }
+}*/
